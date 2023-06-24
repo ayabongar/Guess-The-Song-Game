2f6c2de3797357f859c9b148aedcf8d9fefec3e2
Johan Smit <johansm@bbd.co.za>
2023-06-24 08:16:51 +0200

    Added token expiration and EB config

--- IdentityProvider/app.js
+++ IdentityProvider/app.js
@@ -18,12 +18,14 @@ App.use(cors({
 App.use(express.json());
 App.use(cookieParser());
 
-const Port = process.env.Port || 8040;
+const Port = process.env.PORT || 8081;
 
 
 App.post("/authenticate", (req, res) => {
 
     const { username, password } = req.body;
+
+    console.log(process.env.JWT_EXPIRATION_TIME);
 
     if (username == "admin" && password == "admin") {
         res.cookie('token', jsonwebtoken.sign({ user: "admin" }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION_TIME}), { httpOnly: true });
@@ -39,14 +41,13 @@ App.post("/authenticate", (req, res) => {
 
 App.post("/verify", (req, res) => {
 
-    if (!req.headers.authorization)
+    if (!req.body.token)
         return res
             .status(401)
-            .json({ error: "Not Authorized: Authorization Header Not Set" });
+            .json({ error: "Not Authorized: Authorization Token Not Sent" });
     
     try {
-        const authHeader = req.headers.authorization;
-        const token = authHeader.split(" ")[0];
+        const token = req.body.token;
 
         const { user } = jsonwebtoken.verify(token, process.env.JWT_SECRET);
         return res
