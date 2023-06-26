545b76de30f88d4a173cb8584d7544fd3995ed9f
Johan Smit <johansm@bbd.co.za>
2023-06-26 21:51:35 +0200

    temp

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,6 @@
 module.exports = (app) => {
-    const userController = require("../controllers/userController");
     const staticResourceController = require("../controllers/StaticResourceController")
     
-    //Game
-    app.get("/users", userController.getAllUsers);
-    app.get("/past-scores", userController.getPastScores);
-    app.get("/score", userController.getScore);
-
-
     /* Pages */
     app.get("/", staticResourceController.getHomePage);
     app.get("/authenticate", staticResourceController.getAuthPage);
