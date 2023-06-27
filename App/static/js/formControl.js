b7f06f317f37fdfcc906666fae06080d4932b44c
Johan-Smit <109282750+Johan-Smit@users.noreply.github.com>
2023-06-27 16:10:05 +0200

    Delete App directory

--- App/static/js/formControl.js
+++
@@ -1,9 +0,0 @@
-async function authenticateLogin() {
-    const username = document.getElementById("loginUsername").value
-    const password = document.getElementById("loginPassword").value
-
-    const result = await authenticate(username, password);
-
-    if (result)
-        window.location.href = "/home"
-}
